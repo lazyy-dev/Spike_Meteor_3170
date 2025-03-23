fe00170ba74fd3e5a0bbfe1b4fce256e618690ea
Daksh Malhotra <82654193+C0d3rBr3ak3r@users.noreply.github.com>
2025-03-23 17:45:32 +1100

    Completed the spike, published the data and subscribed to retrieve

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Task } from './Task';
+import React from "react";
+import { useTracker, useSubscribe } from 'meteor/react-meteor-data'; 
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./Task";
 
-const tasks = [
-  {_id: 1, text: 'Test 1'},
-  {_id: 2, text: 'Test 2'},
-  {_id: 3, text: 'Test 3'},
-];
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
+export const App = () => {
 
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+  const isLoading = useSubscribe("tasks"); 
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
