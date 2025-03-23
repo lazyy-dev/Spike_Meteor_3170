4eb5cad1d8b9a31779cc4ca3ed7e6b815c705200
Daksh Malhotra <82654193+C0d3rBr3ak3r@users.noreply.github.com>
2025-03-23 17:18:08 +1100

    Created a test task file and updated app.jsx respectively

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 
+const tasks = [
+  {_id: 1, text: 'Test 1'},
+  {_id: 2, text: 'Test 2'},
+  {_id: 3, text: 'Test 3'},
+];
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
