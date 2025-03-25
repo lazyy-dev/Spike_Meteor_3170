64d7f005623efef2c789163c7214f40766490438
Daksh Malhotra <82654193+C0d3rBr3ak3r@users.noreply.github.com>
2025-03-26 05:29:48 +1100

    Completed step 3

--- simple-todos-react/client/main.jsx
+++ simple-todos-react/client/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
 
+
+import "../imports/api/tasksMethods"; 
+
+
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { useTracker, useSubscribe } from 'meteor/react-meteor-data'; 
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
+import { TaskForm } from "./TaskForm";
 
 export const App = () => {
 
-  const isLoading = useSubscribe("tasks"); 
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  
 
+  const isLoading = useSubscribe("tasks"); 
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  if (isLoading()) {
+    return <div>Loading...</div>;
+  }
+  
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
 
       <ul>
         {tasks.map((task) => (
